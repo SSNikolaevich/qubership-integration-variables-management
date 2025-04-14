448bf02f36af7e42cd4808afeec027e589af45f0
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2025-04-14 11:41:06 +0300

    Moved logic of resolving of a secret name to separate class

--- src/test/java/org/qubership/integration/platform/variables/management/service/SecuredVariablesServiceTest.java
+++ src/test/java/org/qubership/integration/platform/variables/management/service/SecuredVariablesServiceTest.java
@@ -56,6 +56,7 @@ import static org.mockito.Mockito.*;
 @ContextConfiguration(classes = {
         MapperAutoConfiguration.class,
         KubeSecretSerializer.class,
+        SecretNameResolverService.class,
         SecuredVariableService.class
 })
 @ExtendWith(SpringExtension.class)
